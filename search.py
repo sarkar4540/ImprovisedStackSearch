78ae55d289426d2d618297131dc7b0a503f05b41
Aniruddha Sarkar <sarkar4540@india.com>
2019-06-18 11:36:17 +0530

    - Now uses single api call for multiple ids in stackoverflow - Now makes a list of top k questions and provides best answer for the question instead of giving top k answers only - Minor bugs fixes

--- search.py
+++ search.py
@@ -1,11 +1,11 @@
 import json
 import re
-from flask import Flask
 
+import networkx as nx
+from flask import Flask
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions, SentimentOptions
 from stackapi import StackAPI
-import networkx as nx
 
 nlu = NaturalLanguageUnderstandingV1(
     iam_apikey="1A3R_SmipU_wkdOjJRwSxZPmn6dIgriROn4M6zngTR3v", version="2018-11-16",
@@ -42,25 +42,25 @@ def separate_elements(array):
     return list_a
 
 
-def get_questions_stackoverflow(query, mincount):
+def get_questions_stackoverflow(query):
     """Fetches the questions from Stackoverflow API using the query provided"""
     stack = StackAPI("stackoverflow")
-    res = stack.fetch("search/advanced", q=query, sort="relevance", accepted=True, pagesize=mincount, max_pages=1,
+    res = stack.fetch("search/advanced", q=query, sort="relevance", accepted=True,
                       filter="withbody")
     return res
 
 
-def get_answers_stackoverflow(question_id):
+def get_answers_stackoverflow(question_ids):
     """Fetches the answers from Stackoverflow API using the question_id provided"""
     stack = StackAPI("stackoverflow")
-    res = stack.fetch("questions/{ids}/answers", ids=[question_id], sort="votes", filter="withbody")
+    res = stack.fetch("questions/{ids}/answers", ids=question_ids, sort="votes", filter="withbody")
     return res
 
 
-def get_comments_stackoverflow(answer_id):
+def get_comments_stackoverflow(answer_ids):
     """Fetches the comments from Stackoverflow API using the answer_id provided"""
     stack = StackAPI("stackoverflow")
-    res = stack.fetch("answers/{ids}/comments", ids=[answer_id], sort="creation", filter="withbody")
+    res = stack.fetch("answers/{ids}/comments", ids=answer_ids, sort="creation", filter="withbody")
     return res
 
 
@@ -69,84 +69,100 @@ def analyse_sentiment(sentence):
     response = nlu.analyze(
         text=sentence,
         language="en",
-        sentiment=SentimentOptions()).get_result()
+        features=Features(sentiment=SentimentOptions())).get_result()
     return float(response["sentiment"]["document"]["score"])
 
 
-@app.route("/<int:answer_limit>/<question>")
-def search(answer_limit, question):
-    question_tags = get_keywords(question)
+@app.route("/<int:answer_limit>/<query>")
+def search(answer_limit, query):
+    question_tags = get_keywords(query)
     print("Extracted tags: ", end="")
     print(question_tags)
+    result_json_q = get_questions_stackoverflow(space_separated_elements(question_tags))
+    if len(result_json_q["items"]) < answer_limit:
+        for i in range(0, len(question_tags) - 2):
+            result_json_q["items"].extend(get_questions_stackoverflow(
+                str(question_tags[i]) + " " + str(question_tags[i + 1]) + " " + str(question_tags[i + 2])))
+
+    print("Got " + str(len(result_json_q["items"])) + " questions... processing.")
+    questions_tags = {}
     edges = []
     nodes = []
     questions = {}
-    result_json_q = (get_questions_stackoverflow(
-        space_separated_elements(question_tags), answer_limit))
-    if "items" in result_json_q:
-        print("Got " + str(len(result_json_q["items"])) + " questions... processing.")
-        questions_tags = {}
+    if len(result_json_q["items"]) > 0:
         for question_b in result_json_q["items"]:
-            tags = list(set(get_keywords(question_b["title"])) | set(question_b["tags"]))
-            questions_tags[question_b["question_id"]] = tags
-            questions[question_b["question_id"]] = question_b;
-            for tag1 in tags:
-                if not (tag1 in nodes):
-                    nodes.append(tag1)
-                for tag2 in tags:
-                    if not (tag1 is tag2):
-                        edges.append((tag1, tag2))
-    print("Ranking questions... ", end="")
-    graph = nx.Graph()
-    graph.add_nodes_from(nodes)
-    graph.add_edges_from(edges)
-    probable_paths = []
-    for source in re.findall(r'\w+', question):
-        for destination in question_tags:
-            if not (source is destination) and (source in nodes) and (destination in nodes):
-                probable_paths.extend(nx.all_shortest_paths(graph, source, destination))
+            if isinstance(question_b, dict):
+                tags = list(set(get_keywords(question_b["title"])) | set(question_b["tags"]))
+                questions_tags[question_b["question_id"]] = tags
+                questions[question_b["question_id"]] = question_b
+                questions[question_b["question_id"]]["answer_scores"] = {}
+                for tag1 in tags:
+                    if not (tag1 in nodes):
+                        nodes.append(tag1)
+                    for tag2 in tags:
+                        if not (tag1 is tag2):
+                            edges.append((tag1, tag2))
+        print("Ranking questions... ")
+        graph = nx.Graph()
+        graph.add_nodes_from(nodes)
+        graph.add_edges_from(edges)
+        probable_paths = []
+        for source in re.findall(r'\w+', query):
+            for destination in question_tags:
+                if not (source is destination) and (source in nodes) and (destination in nodes):
+                    probable_paths.extend(nx.all_shortest_paths(graph, source, destination))
+        question_scores = {}
+        for question_b in questions.values():
+            score = 0.0
+            tag_count = 0.0
+            for path in probable_paths:
+                tags = questions_tags.get(question_b["question_id"])
+                for tag in tags:
+                    if tag in path:
+                        score = score + 1
+                    tag_count = tag_count + 1
+            question_scores[question_b["question_id"]] = 0 if tag_count == 0 else score / tag_count
 
-    question_scores = {}
-    for question_b in result_json_q["items"]:
-        score = 0.0
-        tag_count = 0.0
-        for path in probable_paths:
-            tags = questions_tags.get(question_b["question_id"])
-            for tag in tags:
-                if tag in path:
-                    score = score + 1
-                tag_count = tag_count + 1
-        question_scores[question_b["question_id"]] = 0 if tag_count == 0 else score / tag_count
-    print("Done.")
-    print("Ranking answers based on comments...", end="")
-    index = 0
-    answer_scores = {}
-    answers = {}
-    for key in sorted(question_scores, key=question_scores.get):
-        t_answer = get_answers_stackoverflow(key)
-        for answer in t_answer["items"]:
+        print("Done.")
+        print("Ranking answers based on comments...")
+        answers = {}
+        questions_sorted = sorted(question_scores, key=question_scores.get, reverse=True)[:answer_limit]
+        result_json_a = get_answers_stackoverflow(questions_sorted)
+        for answer in result_json_a["items"]:
+            answers[answer["answer_id"]] = answer
+        result_json_c = get_comments_stackoverflow(answers.keys())
+        comments = {}
+        for comment in result_json_c["items"]:
+            if comment["post_id"] in comments:
+                comments[comment["post_id"]].append(comment)
+            else:
+                comments[comment["post_id"]] = [comment]
+
+        for answer in answers.values():
             score = 0.0
             count = 0
-            t_comments = get_comments_stackoverflow(key)
-            for comment in t_comments["items"]:
-                score = score + analyse_sentiment(comment)
-                count = count + 1
-            answer_scores[answer["answer_id"]] = 0 if count == 0 else score / count
-            answers[answer["answer_id"]] = answer
+            if answer["answer_id"] in comments.keys():
+                t_comments = comments[answer["answer_id"]]
+                for comment in t_comments:
+                    score = score + analyse_sentiment(comment["body"])
+                    count = count + 1
+            questions[answer["question_id"]]["answer_scores"][answer["answer_id"]] = 0 if count == 0 else score / count
+
+        print("Done.")
+        print("Picking top answers for questions...")
+
+        results = []
+        index = 0
+        for question_id in questions_sorted:
+            question = questions[question_id]
+            answer_id = max(question["answer_scores"], key=question["answer_scores"].get)
+            results.append({"index": index, "question": question, "answer": answers[answer_id]})
             index = index + 1
-            if index >= answer_limit:
-                break
-        if index >= answer_limit:
-            break
-    print("Done.")
-    print("Answers found:")
-    results = []
-    for key in sorted(answer_scores, key=answer_scores.get):
-        results.append((questions[answers[key]["question_id"]]["title"], answers[key]["body"]))
+        print("Done.")
 
-    return json.dumps(results)
+        return json.dumps(results)
 
 
-question = input("Enter the search string:")
-k = input("Enter number of answers:")
-print(search(int(k), question))
+questionq = input("Enter the search string:")
+k = input("Enter number of answers(max 50):")
+print(search(50 if int(k) > 50 else int(k), questionq))
