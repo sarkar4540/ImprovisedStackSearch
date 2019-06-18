c8971a192f4c6cb517e6e3553f166f7f2da11a93
Aniruddha Sarkar <sarkar4540@india.com>
2019-06-18 16:47:11 +0530

    Converted Python3 to Python3/2 compatible

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from builtins import input
+from builtins import int
+
+from future import standard_library
+
+standard_library.install_aliases()
 import search
 
 questionq = input("Enter the search string:")
---
+++ run.py.bak
@@ -0,0 +1,5 @@
+import search
+
+questionq = input("Enter the search string:")
+k = input("Enter number of answers(max 50):")
+print(search.search(answer_limit=(50 if int(k) > 50 else int(k)), query=questionq))
